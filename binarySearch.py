8d767985acfa69b58f6d1ff0ca405958b2943b53
abu-samRah <abdallahabuamrah@gmail.com>
2021-01-11 18:44:19 +0300

    adding checking for multible occurances to binary search

--- binarySearch.py
+++ binarySearch.py
@@ -1,27 +1,32 @@
-def binary_search_recursive(numbers_list, number_to_find, left_index, right_index):
-    if right_index < left_index:
-        return -1
-
-    mid_index = (left_index + right_index) // 2
-    if mid_index >= len(numbers_list) or mid_index < 0:
-        return -1
-
-    mid_number = numbers_list[mid_index]
-
-    if mid_number == number_to_find:
-        return mid_index
-
-    if mid_number < number_to_find:
-        left_index = mid_index + 1
-    else:
-        right_index = mid_index - 1
-
-    return binary_search_recursive(numbers_list, number_to_find, left_index, right_index)
-
+def binary_search_recursive(numbers_list, number_to_find, left_index, right_index, indexs=[]):
+   if right_index < left_index:
+       return -1
+   
+   med_index = (left_index + right_index)//2
+   if med_index >= len(numbers_list) or med_index < 0:
+       return -1
+   
+   med_element = numbers_list[med_index]
+   
+   if number_to_find < med_element:
+      right_index = med_index -1
+      
+    
+   elif number_to_find > med_element:
+       left_index = med_index + 1
+       
+   else:
+       numbers_list.pop(med_index)
+       left_index = 0
+       right_index = len(numbers_list) -1
+       indexs.append(med_index)
+       
+   binary_search_recursive(numbers_list, number_to_find, left_index, right_index,indexs)
+   return indexs
 
 if __name__ == '__main__':
-    numbers_list = [12, 15, 17, 19, 21]
-    number_to_find = 15
+    numbers_list = [15, 15,17, 17,19, 21,21]
+    number_to_find = 21
 
-    index = binary_search_recursive(numbers_list, number_to_find, 0, len(numbers_list))
+    index = binary_search_recursive(numbers_list, number_to_find, 0, len(numbers_list)-1)
     print(f"Number found at index {index} using binary search")
